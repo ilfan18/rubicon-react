d2a04541cdf653a31c2099b6ea7b65f5cb2e6afa
ilfan18 <ilfanmuratov@gmail.com>
2023-01-10 13:35:37 +0500

    .

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,29 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
-  // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
-    });
+	stories: ['../src/**/*.stories.tsx'],
+	// Add any Storybook addons you want here: https://storybook.js.org/addons/
+	addons: [
+		'storybook-addon-sass-postcss',
+		{
+			name: 'storybook-addon-sass-postcss',
+			options: {
+				sassLoaderOptions: {
+					implementation: require('sass'),
+				},
+			},
+		},
+	],
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			loader: require.resolve('babel-loader'),
+			options: {
+				presets: [['react-app', { flow: false, typescript: true }]],
+			},
+		});
+		config.resolve.extensions.push('.ts', '.tsx');
 
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
-      options: {
-        presets: [["react-app", { flow: false, typescript: true }]]
-      }
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-
-    return config;
-  }
+		return config;
+	},
 };
