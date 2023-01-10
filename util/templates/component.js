aa3cf78dd373018af1086de5ae659adc1d0eb52a
ilfan18 <ilfanmuratov@gmail.com>
2023-01-10 15:15:25 +0500

    Fixing templates

--- util/templates/component.js
+++ util/templates/component.js
@@ -1,17 +1,20 @@
 module.exports = (componentName) => ({
-  content: `// Generated with util/create-component.js
+	content: `// Generated with util/create-component.js
 import React from "react";
+import styled from "styled-components";
 
 import { ${componentName}Props } from "./${componentName}.types";
 
-import "./${componentName}.scss";
+const StyledDiv = styled.div\`
+
+\`;
 
 const ${componentName}: React.FC<${componentName}Props> = ({ foo }) => (
-    <div data-testid="${componentName}" className="foo-bar">{foo}</div>
-);
+    <StyledDiv data-testid="${componentName}" className="foo-bar">{foo}</StyledDiv>
+); 
 
 export default ${componentName};
 
 `,
-  extension: `.tsx`
+	extension: `.tsx`,
 });
