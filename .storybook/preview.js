78b84f1ccb23df1687ed29fd805ee81d4fdaeb2b
ilfan18 <ilfanmuratov@gmail.com>
2023-01-16 13:34:26 +0500

    Adding custom viewport sizes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,70 +2,43 @@ import '../src/reset.css';
 import '../src/index.css';
 import '../src/colors.css';
 import '../src/fonts.css';
-/**
- * Read https://storybook.js.org/docs/react/configure/overview#configure-story-rendering
- * for more information about the purpose of this file.
- *
- * Use preview.js for global code (such as CSS imports or JavaScript mocks)
- * that applies to all stories. For example, `import thirdPartyCss.css`.
- *
- * This file can have three exports:
- * - decorators - an array of global decorators
- * - parameters - an object of global parameters
- * - globalTypes - definition of globalTypes
- */
+import '../src/typography.css';
 
-/**
- * Decorators
- *
- * A decorator is a way to wrap a story in extra “rendering” functionality.
- *
- * Example:
- *
- * import React from 'react';
- * export const decorators = [(Story) => <div style={{ margin: '3em' }}><Story/></div>];
- *
- * Each story throughout the library will be wrapped in a div with a margin of 3
- */
+const customViewports = {
+	Base: {
+		name: 'Base < 768px',
+		styles: {
+			width: '650px',
+			height: '100%',
+		},
+	},
+	Middle: {
+		name: 'Middle >= 768px',
+		styles: {
+			width: '850px',
+			height: '100%',
+		},
+	},
+	Large: {
+		name: 'Large >= 992px',
+		styles: {
+			width: '1050px',
+			height: '100%',
+		},
+	},
+	XL: {
+		name: 'XL >= 1200px',
+		styles: {
+			width: '1250px',
+			height: '100%',
+		},
+	},
+};
 
-/**
- * Parameters
- *
- * Most Storybook addons are configured via a parameter-based API.
- * You can set global parameters in this file
- *
- * export const parameters = {
- *   backgrounds: {
- *     values: [
- *       { name: 'red', value: '#f00' },
- *       { name: 'green', value: '#0f0' },
- *     ],
- *   },
- * };
- *
- * With backgrounds, you configure the list of backgrounds that every story can render in.
- */
-
-/**
- * Global Types
- *
- * Global Types allow you to add your own toolbars by creating
- * globalTypes with a toolbar annotation:
- *
- * For example:
- *
- * export const globalTypes = {
- *   theme: {
- *     name: 'Theme',
- *     description: 'Global theme for components',
- *     defaultValue: 'light',
- *     toolbar: {
- *       icon: 'circlehollow',
- *       // array of plain string values or MenuItem shape
- *       items: ['light', 'dark'],
- *       },
- *     },
- *   };
- *
- * Will add a new dropdown in your toolbar with options light and dark.
- **/
+export const parameters = {
+	viewport: {
+		viewports: {
+			...customViewports,
+		},
+	},
+};
